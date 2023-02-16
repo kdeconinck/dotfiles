ca848b9251574ce4bac84f4f842dda55944640d9
Kevin De Coninck <kevin.dconinck@gmail.com>
2023-02-16 22:23:58 +0100

    Install: Fonts
    - Add a PowerShell module to install fonts.
    - Install the font `Comic Mono`.
    - Install the font `Comic Mono NF`.

---
+++ windows/app/scripts/modules/Fonts/Fonts.psm1
@@ -0,0 +1,36 @@
+# ======================================================================================================================
+# == LICENSE:       Copyright (c) 2023 Kevin De Coninck
+# ==
+# ==                Permission is hereby granted, free of charge, to any person
+# ==                obtaining a copy of this software and associated documentation
+# ==                files (the "Software"), to deal in the Software without
+# ==                restriction, including without limitation the rights to use,
+# ==                copy, modify, merge, publish, distribute, sublicense, and/or sell
+# ==                copies of the Software, and to permit persons to whom the
+# ==                Software is furnished to do so, subject to the following
+# ==                conditions:
+# ==
+# ==                The above copyright notice and this permission notice shall be
+# ==                included in all copies or substantial portions of the Software.
+# ==
+# ==                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# ==                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# ==                OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# ==                NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# ==                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# ==                WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# ==                FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# ==                OTHER DEALINGS IN THE SOFTWARE.
+# ======================================================================================================================
+
+# Load all the files that contains the functions.
+$PSScriptPath = Split-Path $MyInvocation.MyCommand.Path
+
+# Load the "public" and the "private" functions.
+$publicFunctions = @(Get-ChildItem -Path "$PSScriptPath\public" -Filter *.ps1 -Recurse)
+$privateFunctions = @(Get-ChildItem -Path "$PSScriptPath\private" -Filter *.ps1 -Recurse)
+
+# Dot source each function.
+foreach ($function in $($publicFunctions + $privateFunctions)) {
+    . $function.FullName
+}
